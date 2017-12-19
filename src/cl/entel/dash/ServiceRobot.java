cc9132e011eefaa1cd0d2166bcdaf8cc13f160a3
Moyano Toledo, Robby <robby.moyano.toledo@accenture.com>
2017-12-19 16:42:49 -0300

    Se cambia la leyenda de posicion en el grafico principal

--- src/cl/entel/dash/ServiceRobot.java
+++ src/cl/entel/dash/ServiceRobot.java
@@ -84,7 +84,7 @@ public class ServiceRobot {
 		List<RegistroMes> lista = new ArrayList<RegistroMes>();
 
 		List<RegistroMes> lista2 = new ArrayList<RegistroMes>();
-		ConsultasDashboardImp imp=null;
+		ConsultasDashboardImp imp = null;
 		try {
 			imp = new ConsultasDashboardImp();
 		} catch (ClassNotFoundException e) {
@@ -94,12 +94,12 @@ public class ServiceRobot {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		lista2=imp.getMes(1, Utilidades.CalendarToString(inicio), Utilidades.CalendarToString(hoy));
-	//	lista2.add(new RegistroMes("28", "28-11-2017", 24));
-		//lista2.add(new RegistroMes("03", "03-12-2017", 60));
-		//lista2.add(new RegistroMes("12", "12-12-2017", 58));
-		
-		
+		lista2 = imp.getMes(1, Utilidades.CalendarToString(inicio),
+				Utilidades.CalendarToString(hoy));
+		// lista2.add(new RegistroMes("28", "28-11-2017", 24));
+		// lista2.add(new RegistroMes("03", "03-12-2017", 60));
+		// lista2.add(new RegistroMes("12", "12-12-2017", 58));
+
 		while (!inicio.equals(hoy)) {
 
 			String aux = Utilidades.CalendarToString(inicio);
@@ -111,69 +111,63 @@ public class ServiceRobot {
 		String aux = Utilidades.CalendarToString(hoy);
 		lista.add(new RegistroMes(aux.substring(0, 2), aux, 0));
 
-		
-		
-
 		int j = 0;
-		for (int i = 0; i < lista.size(); i++) { //se asume q lista 2 trae al menos 1 registro
+		for (int i = 0; i < lista.size(); i++) { // se asume q lista 2 trae al
+													// menos 1 registro
 			if (lista.get(i).getFecha().equals(lista2.get(j).getFecha())) {
 
 				lista.get(i).setCantidad(lista2.get(j).getCantidad());
 				j++;
-				if(j==lista2.size())break;
+				if (j == lista2.size())
+					break;
 			}
-		}
-
-		for (int i = 0; i < lista.size(); i++) {
-			log.info(lista.get(i).toString());
 		}
 
 		// hasta aqui tenemos la lista con 1 mes completo, debemos compararla
 		// con la del sp
 
 		GraficoMes mes = new GraficoMes();
-		mes.setSubTitle("Del "+Utilidades.CalendarToString(inicio)+" al "+Utilidades.CalendarToString(hoy));
+		mes.setSubTitle("Del " + Utilidades.CalendarToString(inicio) + " al "
+				+ Utilidades.CalendarToString(hoy));
 
 		String categorias[] = new String[lista.size()];
 		for (int i = 0; i < lista.size(); i++) {
-			categorias[i]=lista.get(i).getDia();
+			categorias[i] = lista.get(i).getDia();
 		}
-		
+
 		int dataManual[] = new int[lista.size()];
 		int AcumuladoManual[] = new int[lista.size()];
-		int dataRobot[]= new int[lista.size()];
-		int dataRobotAux[]= new int[lista.size()];
-		int acu=0;
-		int acuBot=0;
+		int dataRobot[] = new int[lista.size()];
+		int dataRobotAux[] = new int[lista.size()];
+		int acu = 0;
+		int acuBot = 0;
 		for (int i = 0; i < lista.size(); i++) {
-			dataManual[i]=lista.get(i).getCantidad();
-			dataRobotAux[i]= (int) (dataManual[i]/Utilidades.getRandomBot(categorias[i]));
-			acu=acu+dataManual[i];
-			AcumuladoManual[i]=acu;
-			acuBot=acuBot+dataRobotAux[i];
-			dataRobot[i]=acuBot;
+			dataManual[i] = lista.get(i).getCantidad();
+			dataRobotAux[i] = (int) (dataManual[i] / Utilidades
+					.getRandomBot(categorias[i]));
+			acu = acu + dataManual[i];
+			AcumuladoManual[i] = acu;
+			acuBot = acuBot + dataRobotAux[i];
+			dataRobot[i] = acuBot;
 		}
-		
 
 		String fechas[] = new String[lista.size()];
 		for (int i = 0; i < lista.size(); i++) {
-			fechas[i]=lista.get(i).getFecha();
-			log.info("data aux robot: "+dataRobotAux[i]);
+			fechas[i] = lista.get(i).getFecha();
 		}
-		
-		
+
 		mes.setFechas(fechas);
 		mes.setCategorias(categorias);
 		mes.setDataManual(dataManual);
 		mes.setDataAcumulado(AcumuladoManual);
 		mes.setDataRobot(dataRobot);
-		
+
 		return mes;
 	}
 
 	public static void main(String args[]) throws ClassNotFoundException,
 			SQLException {
-System.out.println(Utilidades.getRandomBot("01"));
+		System.out.println(Utilidades.getRandomBot("01"));
 	}
 
 }
